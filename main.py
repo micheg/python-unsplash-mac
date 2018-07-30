1de6d467b10ae94e6c1b17e4da8a4bf8494d6f80
rokity <riccardo.amadio1@gmail.com>
2018-07-30 13:56:40 +0200

    finish

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+import time
+import urllib.request
+from appscript import app, mactypes
+from random import randint
+import os
+
+
+
+def set_desktop_background(file):
+    app('Finder').desktop_picture.set(mactypes.File(file))
+
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+                    (percent, progress_size / (1024 * 1024), speed, duration))
+    sys.stdout.flush()
+    
+
+def save(url):    
+    random = randint(0, 9)
+    filename = str(random)+".jpg"
+    urllib.request.urlretrieve(url, filename, reporthook)
+    print("\nfinito")
+    set_desktop_background(filename)
+    for i in range(0,10):
+        if os.path.isfile(str(i)+".jpg") and i!=random:
+            os.remove(str(i)+".jpg")
+
+#import os
+#os.remove("image.jpg")
+save("https://source.unsplash.com/random",)
+
