d9fd33a549dc80d869212cfe12fc817affafa6de
rokity <riccardo.amadio1@gmail.com>
2018-09-12 16:53:57 +0200

    after change desktop bacoground the script restart Dock

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import urllib.request
 from appscript import app, mactypes
 from random import randint
 import os
-
+import subprocess
 
 
 def set_desktop_background(file):
     se = app('System Events')
     desktops = se.desktops.display_name.get()
-    #app('Finder').desktop_picture.set(mactypes.File(file))
-    for d in desktops:    
-        desk = se.desktops[d]    
+    app('Finder').desktop_picture.set(mactypes.File(file))
+    for d in desktops:
+        desk = se.desktops[d]
         desk.picture.set(mactypes.File(file))
+    subprocess.check_call("killall Dock", shell=True)
+
 
 def reporthook(count, block_size, total_size):
     global start_time
@@ -25,21 +27,20 @@ def reporthook(count, block_size, total_size):
     speed = int(progress_size / (1024 * duration))
     percent = int(count * block_size * 100 / total_size)
     sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
-                    (percent, progress_size / (1024 * 1024), speed, duration))
+                     (percent, progress_size / (1024 * 1024), speed, duration))
     sys.stdout.flush()
-    
 
-def save(url):    
+
+def save(url):
     random = randint(0, 1)
     filename = str(random)+".jpg"
     urllib.request.urlretrieve(url, filename, reporthook)
     print("\ndone")
     set_desktop_background(filename)
-    for i in range(0,2):
+    for i in range(0, 2):
        if os.path.isfile(str(i)+".jpg") and i != random:
            os.remove(str(i)+".jpg")
     set_desktop_background(filename)
 
 
 save("https://source.unsplash.com/random",)
-
